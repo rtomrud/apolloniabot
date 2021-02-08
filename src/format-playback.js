4a8380c89d6b6840e56aa1c1341237c201ef0888
Artem Rudenko <rtomrud@gmail.com>
2021-02-08 17:47:57 +0100

    Fix race condition in DisTube on get currentTime
    The dispatcher may be null so we have to add a guard.

--- src/format-playback.js
+++ src/format-playback.js
@@ -1,6 +1,11 @@
+const { formatDuration } = require("distube/src/duration.js");
+
 module.exports = function ({
-  formattedCurrentTime,
+  beginTime,
+  dispatcher,
   songs: [{ formattedDuration, name, url }],
 }) {
+  const currentTime = dispatcher ? dispatcher.streamTime : 0 + beginTime;
+  const formattedCurrentTime = formatDuration(currentTime);
   return `[${name}](${url}) [${formattedCurrentTime}/${formattedDuration}]`;
 };
