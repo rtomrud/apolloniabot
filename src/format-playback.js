6711c8de772a6e0e3e39a31e97167b36c97f0e43
Artem Rudenko <rtomrud@gmail.com>
2021-02-10 03:11:19 +0100

    Fix playback format wrong current time after seek

--- src/format-playback.js
+++ src/format-playback.js
@@ -1,11 +1,6 @@
-const { formatDuration } = require("distube/src/duration.js");
-
 module.exports = function ({
-  beginTime,
-  dispatcher,
+  formattedCurrentTime,
   songs: [{ formattedDuration, name, url }],
 }) {
-  const currentTime = dispatcher ? dispatcher.streamTime : 0 + beginTime;
-  const formattedCurrentTime = formatDuration(currentTime);
   return `[${name}](${url}) [${formattedCurrentTime}/${formattedDuration}]`;
 };
