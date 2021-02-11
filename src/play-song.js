9418bf80509154f84166b6a8c5811b1db2c46f6e
Artem Rudenko <rtomrud@gmail.com>
2021-02-11 18:03:14 +0100

    Add explicit autoplay info

--- src/play-song.js
+++ src/play-song.js
@@ -2,6 +2,10 @@ const formatSong = require("./format-song.js");
 
 module.exports = function (message, queue, song) {
   message.channel.send({
-    embed: { description: `Playing ${formatSong(song)}` },
+    embed: {
+      description: `${
+        song.user === this.client.user ? "Autoplaying" : "Playing"
+      } ${formatSong(song)}`,
+    },
   });
 };
