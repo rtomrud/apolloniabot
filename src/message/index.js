a03f5cacfe06104524ca9ba9f19e7329a7655ebc
Artem Rudenko <rtomrud@gmail.com>
2021-02-14 14:58:54 +0100

    Add permissions check

--- src/message/index.js
+++ src/message/index.js
@@ -1,6 +1,7 @@
 const aliases = require("./aliases.js");
 
 const prefixRegExp = RegExp(`^(?:lena|<@!?${process.env.CLIENT_ID}>)`, "i");
+const permissions = 0x00004800;
 const separatorRegExp = /\s+/;
 
 const handleDefault = function (message) {
@@ -11,6 +12,13 @@ const handleDefault = function (message) {
 
 module.exports = function (message) {
   if (!prefixRegExp.test(message.content)) {
+    return;
+  }
+
+  if (!message.channel.permissionsFor(this.user).has(permissions)) {
+    message.author.send(
+      "I can't do that because I don't have the Send Messages or Embed Links permissions in that channel"
+    );
     return;
   }
 
