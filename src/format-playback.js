3ad7679ee7774e771c69ce6b16d340d831420eff
Artem Rudenko <rtomrud@gmail.com>
2021-02-16 15:14:30 +0100

    Fix uncaught exception in format-playback

--- src/format-playback.js
+++ src/format-playback.js
@@ -1,6 +1,14 @@
-module.exports = function ({
-  formattedCurrentTime,
-  songs: [{ formattedDuration, name, url }],
-}) {
+const getFormattedCurrentTime = (queue) => {
+  try {
+    // formattedCurrentTime is a getter that may throw
+    return queue.formattedCurrentTime;
+  } catch (error) {
+    return "00:00";
+  }
+};
+
+module.exports = function (queue) {
+  const formattedCurrentTime = getFormattedCurrentTime(queue);
+  const [{ formattedDuration, name, url }] = queue.songs;
   return `[${name}](${url}) [${formattedCurrentTime}/${formattedDuration}]`;
 };
