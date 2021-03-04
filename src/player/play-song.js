c850ccda501995a0b2c590b59496c07169606e2e
Artem Rudenko <rtomrud@gmail.com>
2021-03-04 01:31:44 +0100

    Add autoplaying info when a song plays

--- src/player/play-song.js
+++ src/player/play-song.js
@@ -3,9 +3,8 @@ const formatSong = require("../format-song.js");
 module.exports = function (message, queue, song) {
   message.channel.send({
     embed: {
-      title: "Playing",
+      title: this.client.user === song.user ? "Autoplaying" : "Playing",
       description: formatSong(song),
-      footer: { text: this.client.user === song.user ? "autoplay on" : "" },
     },
   });
 };
