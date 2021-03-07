b06f278b7c1f0b56e71f6b0b8cc72f9f6afa1672
Artem Rudenko <rtomrud@gmail.com>
2021-03-07 16:56:53 +0100

    Drop permissions from who command

--- src/message/who.js
+++ src/message/who.js
@@ -2,25 +2,9 @@ const who = async function (message) {
   return message.channel.send({
     embed: {
       title: "Hi, I'm Lena!",
-      description: `
-I play music. Type \`lena help\` to find out what I can do for you.
-
-I need [permission](https://support.discord.com/hc/en-us/articles/206029707-How-do-I-set-up-Permissions-) to:
-
-- View Channels (Read Messages), so that I can read what you tell me
-- Send Messages, so that I can talk with you
-- Embed Links, so that I can answer your commands
-- Add Reactions, so that I can react to your messages
-- Connect, so that I can join you in a voice channel
-- Speak, so that I can play music for you
-
-Need help? Join my [Discord server](https://discord.gg/wp3HWnUDMa)!
-`,
+      description: `I play music. Type \`lena help\` to find out what I can do for you.`,
     },
   });
 };
 
-module.exports = Object.assign(who, {
-  aliases: ["whois", "info", "perms", "permission", "permissions"],
-  safe: true,
-});
+module.exports = Object.assign(who, { aliases: ["info"], safe: true });
