7aa188f7c0c16612e921abf6d648f071addaee81
Artem Rudenko <rtomrud@gmail.com>
2021-06-15 00:32:13 +0200

    Drop workaround for DisTube's bug
    That bug is fixed in DisTube 3.0.0.

--- src/format-playback.js
+++ src/format-playback.js
@@ -1,16 +1,6 @@
-const getFormattedCurrentTime = (queue) => {
-  try {
-    // formattedCurrentTime is a getter that may throw
-    return queue.formattedCurrentTime;
-  } catch (error) {
-    return "00:00";
-  }
-};
-
 module.exports = function (queue) {
-  const formattedCurrentTime = getFormattedCurrentTime(queue);
   const [{ formattedDuration, name, url }] = queue.songs;
-  return `[${name}](${url}) [${formattedCurrentTime}/${
+  return `[${name}](${url}) [${queue.formattedCurrentTime}/${
     formattedDuration === "00:00" ? "?" : formattedDuration
   }]`;
 };
