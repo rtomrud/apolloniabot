b1a2ccdc58d6ac4d6c2d7e9e89b304b07fbb9936
Artem Rudenko <rtomrud@gmail.com>
2021-07-02 21:01:47 +0200

    Add libtool install to user data

--- main.tf
+++ main.tf
@@ -86,6 +86,7 @@ apt update
 apt upgrade -y
 apt install -y ffmpeg
 apt install -y build-essential
+apt install -y libtool
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 apt install -y nodejs
 EOF
