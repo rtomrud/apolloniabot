20e775035ab35d6cfa780d0eb222dab9697ce39e
Artem Rudenko <rtomrud@gmail.com>
2021-07-04 21:26:48 +0200

    Add gp3 EBS volume

--- main.tf
+++ main.tf
@@ -14,15 +14,6 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "this" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-arm64-server-20210622.1"]
-  }
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_vpc" "this" {
   cidr_block = "172.31.0.0/16"
   tags       = var.tags
@@ -69,6 +60,15 @@ resource "aws_security_group" "this" {
   vpc_id = aws_vpc.this.id
 }
 
+data "aws_ami" "this" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-arm64-server-20210622.1"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_key_pair" "this" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnyZ+jDRfHk7M4s61n+kPAfc5+LmNWD8yxdV0bWEs4yy6csprLph18TRr2T4zwoJG7B/HxBUdoBvuycLPksh9VGvmTPcQlDMDb8HHCInsYpF9HjyqkND+GYmJma5YlO2XXo5fXDJedtBMdDJMk0e2uM7D8qM1Z6hVy0vZF8GJ4Ar2V0/8M+dM8c/UINx03aTSk7eU/w/vC22mB4CZVrSlM3NoQUESKfjWFaXnMpldqxHKgUIeKFdo9HpfP0HUc5ortfJU7DLEhy56FTTaWcnDBqgXLq5B8382HrK+5tBWe6hjz2YWVJN1CGIHruG2JOKJEAmtH2Qpeal20o1h8sA0R6s4z4ibncEkEwj4hvRU/wuzhE40/y6neBH3Hmu0P467lRHRa26879PlBqZqrwUfdA7mM9U4hVT/8fKjswvVkZOY5GBU8rPsRNL6KvwLa738XXDuypA49hawUGF0Qt3So2FQpya6ZqQFiTi+hTgsKjTtUvUrx1mWUUUfBGQcKduU= dev@desktop"
 }
@@ -77,9 +77,17 @@ resource "aws_instance" "this" {
   ami                         = data.aws_ami.this.id
   associate_public_ip_address = true
   credit_specification { cpu_credits = "standard" }
-  instance_type          = "t4g.micro"
-  key_name               = aws_key_pair.this.id
+  instance_type = "t4g.micro"
+  key_name      = aws_key_pair.this.id
+  root_block_device {
+    delete_on_termination = false
+    encrypted             = false
+    tags                  = var.tags
+    volume_size           = 8
+    volume_type           = "gp3"
+  }
   subnet_id              = aws_subnet.this.id
+  tags                   = var.tags
   user_data              = <<EOF
 #!/bin/bash
 apt update
@@ -91,5 +99,4 @@ curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 apt install -y nodejs
 EOF
   vpc_security_group_ids = [aws_security_group.this.id]
-  tags                   = var.tags
 }
