b219f2ab45da0002b71e611dd7e2d285746d9b67
Artem Rudenko <rtomrud@gmail.com>
2021-07-08 23:23:07 +0200

    Refactor help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,4 +1,4 @@
-const { Collection, CommandInteraction } = require("discord.js");
+const { CommandInteraction } = require("discord.js");
 const { default: DisTube } = require("distube");
 
 exports.data = {
@@ -11,16 +11,16 @@ exports.handler = async function (
   distube = new DisTube()
 ) {
   const guild = await distube.client.guilds.fetch(interaction.guildId);
-  const commands = await guild.commands.fetch().catch(() => new Collection());
+  const commands = await guild.commands.fetch();
   return interaction.reply({
     embeds: [
       {
         title: "Lena Bot",
         description: `I play music. [Invite me to your server!](https://discord.com/oauth2/authorize?client_id=${distube.client.user.id}&permissions=2150647808&scope=bot%20applications.commands)`,
         url: "https://discord.gg/wp3HWnUDMa",
-        fields: [...commands].map(([, { name, description }]) => ({
-          name: `/${name}`,
-          value: description,
+        fields: Array.from(commands, ([, command]) => ({
+          name: `/${command.name}`,
+          value: command.description,
         })),
       },
     ],
