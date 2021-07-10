10c520779e5344a6bc27aa81bbb94b13952f7ba4
Artem Rudenko <rtomrud@gmail.com>
2021-07-11 00:11:14 +0200

    Refactor format error

--- src/format-error.js
+++ src/format-error.js
@@ -1,14 +1,13 @@
 module.exports = function (error) {
-  const { message } = error;
-  if (
-    message.endsWith("You do not have permission to join this voice channel.")
-  ) {
-    return "Error: I don't have permission to join your voice channel";
-  }
+  switch (error.errorCode) {
+    case "VOICE_MISSING_PERMS":
+      return "Error: I don't have permission to join your voice channel";
+    default: {
+      if (error.message.includes("[youtube-dl] ERROR: Unsupported URL")) {
+        return "Error: I can't play that URL";
+      }
 
-  if (message.includes("[youtube-dl] ERROR: Unsupported URL")) {
-    return "Error: I can't play that URL";
+      return null;
+    }
   }
-
-  return null;
 };
