9271582d84b1d38d95902ff3af3decdd6ddce231
Artem Rudenko <rtomrud@gmail.com>
2021-08-12 23:59:48 +0200

    Add jest dev dependency

---
+++ test/index.test.js
@@ -0,0 +1,22 @@
+import { spawn } from "child_process";
+import { fileURLToPath } from "url";
+import { expect, test } from "@jest/globals";
+
+const path = fileURLToPath(new URL("../src/index.js", import.meta.url));
+
+test("lenabot", async () => {
+  const lenabot = spawn("node", [path]);
+  const exit = await new Promise((resolve, reject) => {
+    const timeout = setTimeout(resolve, 500);
+    lenabot.on("close", (code) => {
+      clearTimeout(timeout);
+      resolve(code);
+    });
+    lenabot.on("error", (error) => {
+      clearTimeout(timeout);
+      reject(error);
+    });
+  });
+  lenabot.kill("SIGKILL");
+  expect(exit).toBe(undefined);
+});
