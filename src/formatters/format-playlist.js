cc2f1bca4e932b47416fe6c0b1122e11c4023827
Artem Rudenko <rtomrud@gmail.com>
2021-10-12 21:18:08 +0200

    Fix playlist format

--- src/formatters/format-playlist.js
+++ src/formatters/format-playlist.js
@@ -1,5 +1,6 @@
-export default function ({ formattedDuration, name, songs: { length }, url }) {
-  return `[${name}](${url}) (${length} track${
-    length === 1 ? "" : "s"
-  }) [${formattedDuration}]`;
+export default function ({ formattedDuration, songs, url }) {
+  const { length } = songs;
+  return `[${songs[0].name}${
+    length > 1 ? ` and ${length - 1} more track${length === 2 ? "" : "s"}` : ""
+  }](${url}) [${formattedDuration}]`;
 }
