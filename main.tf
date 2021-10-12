672112da24f879fa70531903f01bae8f67f99904
Artem Rudenko <rtomrud@gmail.com>
2021-10-12 22:06:32 +0200

    Bump ubuntu AMI to the latest one

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "aws_ami" "this" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-arm64-server-20210720"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-hirsute-21.04-arm64-server-20210928"]
   }
 }
 
