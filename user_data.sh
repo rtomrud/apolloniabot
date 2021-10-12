67b9639447dc928aa0e24d8e33dcdec5d04d7630
Artem Rudenko <rtomrud@gmail.com>
2021-10-12 17:27:24 +0200

    Add CloudWatch agent to EC2 instance

--- user_data.sh
+++ user_data.sh
@@ -7,3 +7,27 @@ apt install -y libtool
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt install -y nodejs
 ln -s /usr/bin/python3.* /usr/bin/python
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/arm64/latest/amazon-cloudwatch-agent.deb
+dpkg -i -E ./amazon-cloudwatch-agent.deb
+rm amazon-cloudwatch-agent.deb
+mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
+cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+{
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/home/ubuntu/.pm2/logs/index-err.log",
+            "log_group_name": "ec2-err.log"
+          },
+          {
+            "file_path": "/home/ubuntu/.pm2/logs/index-out.log",
+            "log_group_name": "ec2-out.log"
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
