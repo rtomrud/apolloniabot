89298492b7159b5563f3a8463b7651604161b9f9
Artem Rudenko <rtomrud@gmail.com>
2022-01-30 03:40:48 +0100

    Add error handling to playSong event

--- src/events/player/play-song.js
+++ src/events/player/play-song.js
@@ -1,7 +1,17 @@
 import { Song } from "distube";
 
+const tokenDuration = 15 * 60 * 1000;
+
 export default function playSong(queue, song = new Song()) {
+  if (song.metadata.interaction.createdTimestamp + tokenDuration < Date.now()) {
+    return;
+  }
+
   song.metadata.interaction
     .followUp({ embeds: [{ title: song.name, url: song.url }] })
-    .catch(console.error);
+    .catch((error) => {
+      if (error.message !== "Invalid Webhook Token") {
+        console.error(error);
+      }
+    });
 }
