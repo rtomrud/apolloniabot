44579b235e4834e4d5c4430415e7dc8b99bda430
Artem Rudenko <rtomrud@gmail.com>
2022-01-30 01:55:20 +0100

    Drop finish event handler

--- src/events/player/finish.js
+++
@@ -1,9 +0,0 @@
-import { Queue } from "distube";
-
-export default function finish(queue = new Queue()) {
-  queue.textChannel
-    .send({
-      embeds: [{ description: "I stopped because the queue is finished" }],
-    })
-    .catch(console.error);
-}
--- src/events/player/index.js
+++ src/events/player/index.js
@@ -1,7 +1,6 @@
 import addList from "./add-list.js";
 import addSong from "./add-song.js";
 import error from "./error.js";
-import finish from "./finish.js";
 import initQueue from "./init-queue.js";
 import noRelated from "./no-related.js";
 import playSong from "./play-song.js";
@@ -10,7 +9,6 @@ export default {
   addList,
   addSong,
   error,
-  finish,
   initQueue,
   noRelated,
   playSong,
