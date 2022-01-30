d79a359a88e40a5f69e091cadd5aa981fb62bac0
Artem Rudenko <rtomrud@gmail.com>
2022-01-30 02:48:59 +0100

    Drop Embed Links and Send Messages permissions

--- src/permissions.js
+++ src/permissions.js
@@ -2,8 +2,6 @@ import { Permissions } from "discord.js";
 
 export default [
   Permissions.FLAGS.CONNECT,
-  Permissions.FLAGS.EMBED_LINKS,
-  Permissions.FLAGS.SEND_MESSAGES,
   Permissions.FLAGS.SPEAK,
   Permissions.FLAGS.USE_APPLICATION_COMMANDS,
 ];
