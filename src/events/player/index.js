c986aa166ecd635c194724454b0021a8f7557ba7
Artem Rudenko <rtomrud@gmail.com>
2022-01-30 01:56:22 +0100

    Drop noRelated event handler

--- src/events/player/index.js
+++ src/events/player/index.js
@@ -2,7 +2,6 @@ import addList from "./add-list.js";
 import addSong from "./add-song.js";
 import error from "./error.js";
 import initQueue from "./init-queue.js";
-import noRelated from "./no-related.js";
 import playSong from "./play-song.js";
 
 export default {
@@ -10,6 +9,5 @@ export default {
   addSong,
   error,
   initQueue,
-  noRelated,
   playSong,
 };
