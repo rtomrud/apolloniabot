00433a4a66f675316e217e0c7123a22147727d87
Artem Rudenko <rtomrud@gmail.com>
2022-02-01 04:34:26 +0100

    Add better error messages

--- src/events/player/error.js
+++ src/events/player/error.js
@@ -1,59 +1,40 @@
 import { TextChannel } from "discord.js";
 
-const formatErrorMessage = ({ errorCode, message }) => {
-  switch (errorCode) {
-    case "NOT_IN_VOICE":
-    case "NOT_SUPPORTED_VOICE":
-    case "VOICE_CHANGE_GUILD":
-      return "Error: I can't join you because you're not in a voice channel";
-    case "VOICE_FULL":
-      return "Error: I can't join your voice channel because it's full";
-    case "VOICE_CONNECT_FAILED":
-    case "VOICE_RECONNECT_FAILED":
-      return "Error: I can't connect to your voice channel";
-    case "VOICE_MISSING_PERMS":
-      return "Error: I can't join your voice channel because I don't have permission";
-    case "NO_RELATED":
-    case "CANNOT_PLAY_RELATED":
-      return "Error: I can't play any related track";
-    case "UNAVAILABLE_VIDEO":
-      return "Error: I can't play that because it's unavailable";
-    case "UNPLAYABLE_FORMATS":
-    case "NOT_SUPPORTED_URL":
-    case "CANNOT_RESOLVE_SONG":
-      return "Error: I can't play that";
-    case "NO_VALID_SONG":
-    case "EMPTY_FILTERED_PLAYLIST":
-    case "EMPTY_PLAYLIST":
-      return "Error: I can't play that because there's no valid track";
-    case "NON_NSFW":
-      return "Error: I can't play age-restricted content in a non-NSFW channel";
-    case "INVALID_TYPE":
-      return "Error: I can't play that because it's not a valid URL";
-    default: {
-      if (message.includes("ERROR: Unsupported URL")) {
-        return "Error: I can't play that URL";
-      }
-
-      return null;
-    }
-  }
+const messages = {
+  NOT_IN_VOICE: "I can't join you because you're not in a voice channel",
+  VOICE_FULL: "I can't join your voice channel because it's full",
+  VOICE_CONNECT_FAILED:
+    "I can't join you because I can't connect to your voice channel",
+  VOICE_MISSING_PERMS:
+    "I can't join your voice channel because I don't have permission",
+  VOICE_RECONNECT_FAILED:
+    "I can't join you because I can't reconnect to your voice channel",
+  VOICE_CHANGE_GUILD: "I can't join you because you're in another server",
+  NO_RELATED: "I can't find any related track",
+  CANNOT_PLAY_RELATED: "I can't play any related track",
+  UNAVAILABLE_VIDEO: "I can't play that because it's unavailable",
+  UNPLAYABLE_FORMATS: "I can't play that because it's in an unplayable format",
+  NON_NSFW:
+    "I can't play that because it's age-restricted content and this is a SFW channel",
+  NOT_SUPPORTED_URL: "I can't play that because the website is unsupported",
+  CANNOT_RESOLVE_SONG: "I can't play that because the track is unresolved",
+  EMPTY_FILTERED_PLAYLIST:
+    "I can't play that because there's no valid track or there's only age-restricted content and this is a SFW channel",
+  EMPTY_PLAYLIST: "I can't play that because there's no valid track",
 };
+
+const defaultMessage = "Something went wrong, sorry";
 
 export default function error(
   channel = new TextChannel(),
   error = new Error()
 ) {
-  const message = formatErrorMessage(error);
-  if (!message) {
+  const message = messages[error.errorCode] || defaultMessage;
+  if (message === defaultMessage) {
     console.error(error);
   }
 
   channel.interaction
-    .followUp({
-      embeds: [
-        { description: message || "Error: Something went wrong, sorry" },
-      ],
-    })
+    .followUp({ embeds: [{ description: `Error: ${message}` }] })
     .catch(console.error);
 }
