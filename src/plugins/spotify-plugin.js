3f3a0c191c7fb5220756c9037fe43874383ff0f2
Artem Rudenko <rtomrud@gmail.com>
2022-02-06 03:58:56 +0100

    Optimize Spotify plugin
    Fixes audio stuttering when fetching playlist tracks.

--- src/plugins/spotify-plugin.js
+++ src/plugins/spotify-plugin.js
@@ -1,15 +1,12 @@
 import { CustomPlugin, DisTubeError, Playlist, Song } from "distube";
 import spotifyUri from "spotify-uri";
 import { getTracks } from "spotify-url-info";
+import { search } from "youtube-search-without-api-key";
 
 export default class SpotifyPlugin extends CustomPlugin {
   constructor({ pattern = "spotify" } = {}) {
     super();
     this.pattern = RegExp(pattern);
-    this.searchQuery = (data) =>
-      data.type === "track"
-        ? `${data.artists.map(({ name }) => name).join(" ")} ${data.name}`
-        : `${data.show.name} ${data.name}`;
   }
 
   async validate(url) {
@@ -23,31 +20,37 @@ export default class SpotifyPlugin extends CustomPlugin {
   async play(voiceChannel, url, options) {
     try {
       const tracks = await getTracks(url);
-      if (tracks.length === 1) {
-        const [track] = tracks;
-        const results = await this.distube.search(this.searchQuery(track));
-        this.distube.play(voiceChannel, results[0], options);
+      const songs = await Promise.all(
+        tracks.map(async (data) => {
+          const query =
+            data.type === "track"
+              ? `${data.artists.map(({ name }) => name).join(" ")} ${data.name}`
+              : `${data.show.name} ${data.name}`;
+          const results = await search(query);
+          const [result] = results;
+          return new Song({
+            id: result.id.videoId,
+            url: result.url,
+            name: result.title,
+            duration: result.duration_raw,
+            thumbnail: result.snippet.thumbnails.url,
+            views: result.views,
+          });
+        })
+      );
+      if (songs.length === 1) {
+        this.distube.play(voiceChannel, songs[0], options);
       } else {
-        const searches = await Promise.all(
-          tracks.map((track) =>
-            this.distube.search(this.searchQuery(track)).catch(() => null)
-          )
-        );
-        const songs = searches
-          .filter((results) => results != null)
-          .map((results) => new Song(results[0]));
-        if (songs.length === 0) {
-          throw new DisTubeError("NO_RESULT");
-        }
-
-        const playlist = new Playlist(songs, {
-          properties: { url },
-          metadata: { interaction: options.metadata.interaction },
-        });
+        const playlist = new Playlist(songs, { properties: { url } });
         this.distube.play(voiceChannel, playlist, options);
       }
     } catch (error) {
-      this.distube.emitError(error, options.textChannel);
+      console.error(error);
+      this.distube.emit(
+        "error",
+        options.textChannel,
+        new DisTubeError("NO_RESULT")
+      );
     }
   }
 }
