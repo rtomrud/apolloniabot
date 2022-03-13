0267ad9a6df623e92361566987253c9bc12345e0
Artem Rudenko <rtomrud@gmail.com>
2022-03-13 23:03:04 +0100

    Refactor Spotify plugin

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,5 +1,8 @@
 import { CommandInteraction } from "discord.js";
-import { DisTube as Player } from "distube";
+import { DisTube as Player, Playlist, Song } from "distube";
+import spotifyUri from "spotify-uri";
+import { getTracks } from "spotify-url-info";
+import { search } from "youtube-search-without-api-key";
 
 export const data = {
   name: "play",
@@ -23,6 +26,14 @@ const isHttpUrl = (string) => {
   }
 };
 
+const isSpotifyUrl = (url) => {
+  try {
+    return /spotify/.test(url) && spotifyUri.parse(url).type != null;
+  } catch {
+    return false;
+  }
+};
+
 export const handler = async function (
   interaction = new CommandInteraction(),
   player = new Player()
@@ -41,24 +52,51 @@ export const handler = async function (
   }
 
   const [url] = query.split(" ");
-  const reply = interaction.reply({
-    embeds: [
-      {
-        description: `Searching "${
-          isHttpUrl(url)
-            ? url
-            : `[${query}](https://www.youtube.com/results?${new URLSearchParams(
-                { search_query: query }
-              )})`
-        }"`,
-      },
-    ],
+  const searchUrl = isHttpUrl(url)
+    ? url
+    : `[${query}](https://www.youtube.com/results?${new URLSearchParams({
+        search_query: query,
+      })})`;
+  const reply = await interaction.reply({
+    embeds: [{ description: `Searching "${searchUrl}"` }],
   });
-  channel.interaction = interaction;
-  player.play(interaction.member.voice.channel, query, {
+  const options = {
     member: interaction.member,
-    textChannel: channel,
     metadata: { interaction, reply },
-  });
+  };
+  channel.interaction = interaction;
+  if (isSpotifyUrl(url)) {
+    try {
+      const tracks = await getTracks(url);
+      const songs = await Promise.all(
+        tracks.map(async (data) => {
+          const query =
+            data.type === "track"
+              ? `${data.artists.map(({ name }) => name).join(" ")} ${data.name}`
+              : `${data.show.name} ${data.name}`;
+          const [result] = await search(query);
+          return new Song({
+            id: result.id.videoId,
+            url: result.url,
+            name: result.title,
+            duration: result.duration_raw,
+          });
+        })
+      );
+      const songOrPlaylist =
+        songs.length > 1
+          ? new Playlist(songs, { properties: { url } })
+          : songs[0];
+      player.play(interaction.member.voice.channel, songOrPlaylist, options);
+      return reply;
+    } catch (error) {
+      console.error(error);
+      return interaction.reply({
+        embeds: [{ description: "Error: I can't find that" }],
+      });
+    }
+  }
+
+  player.play(interaction.member.voice.channel, url, options);
   return reply;
 };
--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import { DisTube as Player } from "distube";
 import events from "./events/index.js";
 import playerEvents from "./events/player/index.js";
 import intents from "./intents.js";
-import SpotifyPlugin from "./plugins/spotify-plugin.js";
 import YoutubeDlPlugin from "./plugins/youtube-dl-plugin.js";
 
 const client = new Client({ intents });
 
 const player = new Player(client, {
-  plugins: [new YoutubeDlPlugin(), new SpotifyPlugin()],
+  plugins: [new YoutubeDlPlugin()],
   emitNewSongOnly: true,
   leaveOnFinish: true,
   savePreviousSongs: false,
