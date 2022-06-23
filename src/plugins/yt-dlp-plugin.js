f6608578cd83c5c2cfee8e4b3f63312344e760cf
Artem Rudenko <rtomrud@gmail.com>
2022-06-23 02:54:30 +0200

    Drop youtube-dl-exec dependency

--- src/plugins/yt-dlp-plugin.js
+++ src/plugins/yt-dlp-plugin.js
@@ -1,61 +1,57 @@
+import child_process from "child_process";
+import { promisify } from "util";
 import { DisTubeError, ExtractorPlugin, Playlist, Song } from "distube";
-import youtubeDlExec, { create } from "youtube-dl-exec";
+
+const execFile = promisify(child_process.execFile);
 
 export class YtDlpPlugin extends ExtractorPlugin {
   constructor({ binaryPath = "yt-dlp" } = {}) {
     super();
-    this.youtubeDlExec = null;
-    create(binaryPath)("", { version: true }).then(
-      () => {
-        this.youtubeDlExec = create(binaryPath);
-      },
-      () =>
-        youtubeDlExec("", { version: true }).then(() => {
-          this.youtubeDlExec = youtubeDlExec;
-        })
-    );
+    this.binaryPath = binaryPath;
   }
 
   async validate() {
-    return Boolean(this.youtubeDlExec);
+    return Boolean(this.binaryPath);
   }
 
   async getStreamURL(url) {
-    const response = await this.youtubeDlExec(url, {
-      dumpSingleJson: true,
-      noWarnings: true,
-      preferFreeFormats: true,
-    });
-    return response.url;
+    const { stdout } = await execFile(
+      this.binaryPath,
+      [url, "--dump-single-json", "--no-warnings", "--prefer-free-formats"],
+      { windowsHide: true }
+    );
+    const info = JSON.parse(stdout);
+    return info.url;
   }
 
   async resolve(url, { member, metadata }) {
-    const response = await this.youtubeDlExec(url, {
-      dumpSingleJson: true,
-      noWarnings: true,
-      preferFreeFormats: true,
-    }).catch(async (e) => {
+    const { stdout } = await execFile(
+      this.binaryPath,
+      [url, "--dump-single-json", "--no-warnings", "--prefer-free-formats"],
+      { windowsHide: true }
+    ).catch(async ({ stderr }) => {
       // Send a follup now because throwing here doesn't trigger the error event
       await metadata.interaction.fetchReply();
       metadata.interaction
         .followUp({ embeds: [{ description: "Error: I can't play that" }] })
         .catch(console.error);
-      throw new DisTubeError("YTDLP_ERROR", e);
+      throw new DisTubeError("YTDLP_ERROR", stderr);
     });
-    if (Array.isArray(response.entries) && response.entries.length > 0) {
+    const info = JSON.parse(stdout);
+    if (Array.isArray(info.entries) && info.entries.length > 0) {
       return new Playlist(
         {
-          ...response,
-          songs: response.entries.map(
+          ...info,
+          songs: info.entries.map(
             (entry) =>
               new Song(entry, { member, source: entry.extractor, metadata })
           ),
-          source: response.extractor,
+          source: info.extractor,
         },
         { member, metadata }
       );
     }
 
-    return new Song(response, { member, source: response.extractor, metadata });
+    return new Song(info, { member, source: info.extractor, metadata });
   }
 }
