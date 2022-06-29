d06543d1ad57409bd9c6c55d163a8b1850d7a82f
Artem Rudenko <rtomrud@gmail.com>
2022-06-29 03:23:57 +0200

    Drop Terraform outputs

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "this_ecs_cluster_name" {
-  description = "The name of this ECS cluster."
-  value = "${aws_ecs_cluster.this.name}"
-}
-
-output "this_ecs_service_name" {
-  description = "The name of this ECS service."
-  value = "${aws_ecs_service.this.name}"
-}
