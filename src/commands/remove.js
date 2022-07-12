b4db90545f7f2946542dd1a9a797d4838866c2a5
Artem Rudenko <rtomrud@gmail.com>
2022-07-13 00:43:35 +0200

    Fix broken link in output of remove command

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -45,6 +45,6 @@ export const handler = async function (
   }
 
   return interaction.reply({
-    embeds: [{ description: `Removed ${song.name}](${song.url}` }],
+    embeds: [{ description: `Removed [${song.name}](${song.url})` }],
   });
 };
