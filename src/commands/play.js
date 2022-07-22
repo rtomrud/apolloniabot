74b1f6eaec364fc6b16becc16fdf73d6ce3d010f
Artem Rudenko <rtomrud@gmail.com>
2022-07-22 03:51:49 +0200

    Refactor to catch play errors in play command

--- src/commands/play.js
+++ src/commands/play.js
@@ -18,6 +18,31 @@ export const data = new SlashCommandBuilder()
       .setRequired(true)
   );
 
+const errorMessages = {
+  VOICE_FULL: "Error: I can't join your voice channel because it's full",
+  VOICE_CONNECT_FAILED:
+    "Error: I can't join you because I can't connect to your voice channel",
+  VOICE_MISSING_PERMS:
+    "Error: I can't join your voice channel because I don't have permission",
+  NO_RESULT: "Error: I can't find that",
+  UNAVAILABLE_VIDEO: "Error: I can't play that because it's unavailable",
+  UNPLAYABLE_FORMATS:
+    "Error: I can't play that because it's in an unplayable format",
+  NON_NSFW:
+    "Error: I can't play that because it's age-restricted content and this is a SFW channel",
+  NOT_SUPPORTED_URL:
+    "Error: I can't play that because the website is unsupported",
+  CANNOT_RESOLVE_SONG:
+    "Error: I can't play that because the track is unresolved",
+  EMPTY_FILTERED_PLAYLIST:
+    "Error: I can't play that because there's no valid track or there's only age-restricted content and this is a SFW channel",
+  EMPTY_PLAYLIST: "Error: I can't play that because there's no valid track",
+  SPOTIFY_PLUGIN_NO_RESULT: "Error: I can't find that",
+  YTDLP_ERROR: "Error: I can't play that",
+};
+
+const defaultErrorMessage = "Error: Something went wrong, sorry";
+
 const isHttpUrl = (string) => {
   try {
     return new URL(string).protocol.startsWith("http");
@@ -55,10 +80,30 @@ export const handler = async function (
   interaction.interactionResponse = interaction.reply({
     embeds: [{ description: `Searching "${searchUrl}"` }],
   });
-  await player.play(interaction.member.voice.channel, query, {
-    member: interaction.member,
-    textChannel: interaction.channel,
-    metadata: { interaction, source: "yt-dlp" },
-  });
-  return interaction.interactionResponse;
+  return player
+    .play(interaction.member.voice.channel, query, {
+      member: interaction.member,
+      textChannel: interaction.channel,
+      metadata: { interaction, source: "yt-dlp" },
+    })
+    .then(() => interaction.interactionResponse)
+    .catch(async (error) => {
+      if (
+        !errorMessages[error.errorCode] ||
+        error.errorCode === "SPOTIFY_PLUGIN_NO_RESULT" ||
+        error.errorCode === "YTDLP_ERROR"
+      ) {
+        console.error(error);
+      }
+
+      await interaction.interactionResponse;
+      return interaction.followUp({
+        embeds: [
+          {
+            description: errorMessages[error.errorCode] || defaultErrorMessage,
+            color: Colors.Red,
+          },
+        ],
+      });
+    });
 };
--- src/index.js
+++ src/index.js
@@ -88,34 +88,6 @@ client.on("guildDelete", (guild) => {
   );
 });
 
-const errorMessages = {
-  NOT_IN_VOICE: "I can't join you because you're not in a voice channel",
-  VOICE_FULL: "I can't join your voice channel because it's full",
-  VOICE_CONNECT_FAILED:
-    "I can't join you because I can't connect to your voice channel",
-  VOICE_MISSING_PERMS:
-    "I can't join your voice channel because I don't have permission",
-  VOICE_RECONNECT_FAILED:
-    "I can't join you because I can't reconnect to your voice channel",
-  VOICE_CHANGE_GUILD: "I can't join you because you're in another server",
-  NO_RESULT: "I can't find that",
-  NO_RELATED: "I can't find any related track",
-  CANNOT_PLAY_RELATED: "I can't play any related track",
-  UNAVAILABLE_VIDEO: "I can't play that because it's unavailable",
-  UNPLAYABLE_FORMATS: "I can't play that because it's in an unplayable format",
-  NON_NSFW:
-    "I can't play that because it's age-restricted content and this is a SFW channel",
-  NOT_SUPPORTED_URL: "I can't play that because the website is unsupported",
-  CANNOT_RESOLVE_SONG: "I can't play that because the track is unresolved",
-  EMPTY_FILTERED_PLAYLIST:
-    "I can't play that because there's no valid track or there's only age-restricted content and this is a SFW channel",
-  EMPTY_PLAYLIST: "I can't play that because there's no valid track",
-  SPOTIFY_PLUGIN_NO_RESULT: "I can't find that",
-  YTDLP_ERROR: "I can't play that",
-};
-
-const defaultErrorMessage = "Something went wrong, sorry";
-
 client.on("interactionCreate", (interaction) => {
   if (!interaction.type === InteractionType.ApplicationCommand) {
     return;
@@ -146,21 +118,7 @@ client.on("interactionCreate", (interaction) => {
     return;
   }
 
-  command.handler(interaction, player).catch(async (error) => {
-    const errorMessage = errorMessages[error.errorCode] || defaultErrorMessage;
-    if (
-      errorMessage === defaultErrorMessage ||
-      error.errorCode === "SPOTIFY_PLUGIN_NO_RESULT" ||
-      error.errorCode === "YTDLP_ERROR"
-    ) {
-      console.error(error);
-    }
-
-    await interaction.interactionResponse;
-    interaction[interaction.interactionResponse ? "followUp" : "reply"]({
-      embeds: [{ description: `Error: ${errorMessage}`, color: Colors.Red }],
-    }).catch(console.error);
-  });
+  command.handler(interaction, player).catch(console.error);
 });
 
 client.on("ready", async (client) => {
