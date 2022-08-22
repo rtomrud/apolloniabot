f8a2044adab5085042d06b24adada4c050360764
Artem Rudenko <rtomrud@gmail.com>
2022-08-22 17:55:57 +0200

    Add select menu for volume

--- src/commands/volume.ts
+++ src/commands/volume.ts
@@ -1,6 +1,13 @@
 import {
+  ActionRowBuilder,
   ChatInputCommandInteraction,
   Colors,
+  Interaction,
+  InteractionReplyOptions,
+  InteractionType,
+  InteractionUpdateOptions,
+  SelectMenuBuilder,
+  SelectMenuInteraction,
   SlashCommandBuilder,
 } from "discord.js";
 import { DisTube as Player } from "distube";
@@ -17,22 +24,41 @@ export const data = new SlashCommandBuilder()
   );
 
 export const handler = async function (
-  interaction: ChatInputCommandInteraction,
+  interaction: ChatInputCommandInteraction | SelectMenuInteraction,
   player: Player
 ) {
-  const queue = player.queues.get(interaction);
+  const queue = player.queues.get(interaction as Interaction);
   if (!queue) {
     return interaction.reply({
       embeds: [{ description: "Error: Nothing is playing", color: Colors.Red }],
     });
   }
 
-  const volume = interaction.options.getInteger("volume");
-  if (volume) {
+  const volume =
+    interaction.type === InteractionType.ApplicationCommand
+      ? interaction.options.getInteger("volume")
+      : Number(interaction.values[0]);
+  if (volume != null) {
     queue.setVolume(volume);
   }
 
-  return interaction.reply({
-    embeds: [{ description: `Volume: ${queue.volume}` }],
-  });
+  const options = {
+    components: [
+      new ActionRowBuilder<SelectMenuBuilder>().addComponents(
+        new SelectMenuBuilder()
+          .setCustomId("/volume")
+          .setPlaceholder(`Volume: ${queue.volume}`)
+          .addOptions(
+            ["0", "25", "50", "75", "100"].map((value) => ({
+              default: queue.volume === Number(value),
+              label: `Volume: ${value}`,
+              value,
+            }))
+          )
+      ),
+    ],
+  } as InteractionReplyOptions & InteractionUpdateOptions;
+  return interaction.type === InteractionType.ApplicationCommand
+    ? interaction.reply(options)
+    : interaction.update(options);
 };
