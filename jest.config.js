a1487b16cd006981715098fcde9c146dae2fc9e5
Artem Rudenko <rtomrud@gmail.com>
2022-08-30 01:40:17 +0200

    Add @swc dev dependencies
    Because tsc is too slow for both build and for Jest tests with ts-jest.

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,10 @@
 export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
-    "^.+\\.ts$": "ts-jest",
-  }
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
 };
