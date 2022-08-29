2e30a0f4fe41ad349e00ca54b835194ce7b430aa
Artem Rudenko <rtomrud@gmail.com>
2022-08-29 04:05:57 +0200

    Add lexicographic sorting of command names in help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -26,10 +26,14 @@ export const handler = async function (
         .addFields(
           !commands
             ? []
-            : Array.from(commands).map(([, command]) => ({
-                name: `/${command.name}`,
-                value: command.description,
-              }))
+            : Array.from(commands)
+                .sort(([, a], [, b]) =>
+                  a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+                )
+                .map(([, command]) => ({
+                  name: `/${command.name}`,
+                  value: command.description,
+                }))
         ),
     ],
   });
