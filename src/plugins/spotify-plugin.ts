109461777c06a00e902f9101c1b01352370c8dd6
Artem Rudenko <rtomrud@gmail.com>
2022-12-27 02:51:09 +0100

    Refactor SpotifyPlugin

--- src/plugins/spotify-plugin.ts
+++ src/plugins/spotify-plugin.ts
@@ -11,73 +11,13 @@ import { parseDocument } from "htmlparser2";
 import fetch from "node-fetch";
 import { SearchOptions, search } from "scrape-youtube";
 
-type Metadata = {
-  data: {
-    entity:
-      | {
-          type: "episode";
-          title: string;
-          subtitle: string;
-        }
-      | {
-          type: "track";
-          title: string;
-          artists: { name: string }[];
-        }
-      | {
-          type: "artist" | "playlist";
-          title: string;
-          trackList: {
-            title: string;
-            subtitle: string;
-          }[];
-        };
-  };
-};
-
-const spotifyUrlRegExp =
-  /^https?:\/\/(open|play)\.spotify\.com\/(?<type>album|artist|episode|playlist|show|track)\/(?<id>[a-zA-Z0-9]+)\??.*$/;
-
-const getTracks = async (url: string) => {
-  const { type, id } = spotifyUrlRegExp.exec(url)?.groups || {};
-  const embedUrl = `https://embed.spotify.com/?uri=spotify:${type}:${id}`;
-  const response = await fetch(embedUrl);
-  const text = await response.text();
-  const node = findOne(
-    (elem) => elem.type === "script" && elem.attribs.id === "initial-state",
-    parseDocument(text).children
-  );
-  if (!node) {
-    return [];
-  }
-
-  const jsonString = Buffer.from(textContent(node), "base64").toString();
-  const metadata = JSON.parse(jsonString) as Metadata;
-  const { entity } = metadata.data;
-  switch (entity.type) {
-    case "track":
-      return [
-        {
-          title: entity.title,
-          subtitle: entity.artists.map(({ name }) => name).join(" "),
-        },
-      ];
-    case "episode":
-      return [{ title: entity.title, subtitle: entity.subtitle }];
-    case "playlist":
-    case "artist":
-    default:
-      return entity.trackList;
-  }
-};
-
 export class SpotifyPlugin extends ExtractorPlugin {
   regExp: RegExp;
 
   searchOptions: SearchOptions;
 
   constructor({
-    regExp = spotifyUrlRegExp,
+    regExp = /^https?:\/\/(open|play)\.spotify\.com\/(?<type>album|artist|episode|playlist|show|track)\/(?<id>[a-zA-Z0-9]+)\??.*$/,
     searchOptions = { type: "video" } as SearchOptions,
   } = {}) {
     super();
@@ -93,7 +33,7 @@ export class SpotifyPlugin extends ExtractorPlugin {
     url: string,
     { member, metadata }: { member?: GuildMember; metadata?: T }
   ) {
-    const tracks = await getTracks(url).catch((error: Error) => {
+    const tracks = await SpotifyPlugin.getTracks(url).catch((error: Error) => {
       throw new DisTubeError("SPOTIFY_PLUGIN_NO_RESULT", String(error));
     });
     const songs = await Promise.all(
@@ -121,5 +61,49 @@ export class SpotifyPlugin extends ExtractorPlugin {
     return songs.length > 1
       ? new Playlist(songs, { member, properties: { url }, metadata })
       : songs[0];
+  }
+
+  static async getTracks(url: string) {
+    const [, type, id] = new URL(url).pathname.split("/");
+    const embedUrl = `https://embed.spotify.com/?uri=spotify:${type}:${id}`;
+    const response = await fetch(embedUrl);
+    const text = await response.text();
+    const node = findOne(
+      (elem) => elem.type === "script" && elem.attribs.id === "initial-state",
+      parseDocument(text).children
+    );
+    if (!node) {
+      return [];
+    }
+
+    const metadata = JSON.parse(
+      Buffer.from(textContent(node), "base64").toString()
+    ) as {
+      data: {
+        entity:
+          | { type: "episode"; title: string; subtitle: string }
+          | { type: "track"; title: string; artists: { name: string }[] }
+          | {
+              type: "artist" | "playlist";
+              trackList: { title: string; subtitle: string }[];
+            };
+      };
+    };
+    const { entity } = metadata.data;
+    switch (entity.type) {
+      case "track":
+        return [
+          {
+            title: entity.title,
+            subtitle: entity.artists.map(({ name }) => name).join(" "),
+          },
+        ];
+      case "episode":
+        return [{ title: entity.title, subtitle: entity.subtitle }];
+      case "playlist":
+      case "artist":
+      default:
+        return entity.trackList;
+    }
   }
 }
