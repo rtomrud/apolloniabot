ad3260ae7bda1bc36a8ea5f1cf08f682b92dc50b
Artem Rudenko <rtomrud@gmail.com>
2023-04-08 21:11:20 +0200

    Fix Jest not working with undici dependency
    See:
    https://github.com/nodejs/undici/issues/1878#issuecomment-1421484960

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
   moduleNameMapper: {
+    "^(\\.{1,2}/.*\\.wasm\\.js)$": "$1",
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
