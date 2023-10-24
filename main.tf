03d09b063a414bcf96f9145f5bf2265b11fe1081
Artem Rudenko <rtomrud@gmail.com>
2023-10-24 02:05:43 +0200

    Drop public IPv4 from ECS service

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_ecs_service" "this" {
   task_definition = aws_ecs_task_definition.this.arn
 
   network_configuration {
-    assign_public_ip = true
+    assign_public_ip = false
     security_groups  = [aws_security_group.this.id]
     subnets          = aws_subnet.this.*.id
   }
