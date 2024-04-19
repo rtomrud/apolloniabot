beead7397c2b364f548763928d5e552d49339540
Artem Rudenko <rtomrud@gmail.com>
2024-04-19 07:01:32 +0200

    Change build, start and test scripts

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*\\.wasm\\.js)$": "$1",
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
-  transform: {
-    "^.+\\.(t|j)sx?$": "esbuild-jest",
-  },
+  preset: "ts-jest",
 };
