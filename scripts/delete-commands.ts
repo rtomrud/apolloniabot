c227a2050d29cf879339d7fa375ad903164db97e
Artem Rudenko <rtomrud@gmail.com>
2025-06-27 00:28:50 +0200

    Refactor delete-commands script

--- scripts/delete-commands.ts
+++ scripts/delete-commands.ts
@@ -1,9 +1,8 @@
 import "dotenv/config";
 import { REST, Routes } from "discord.js";
 
-const rest = new REST().setToken(process.env.TOKEN || "");
-
-rest
+new REST()
+  .setToken(process.env.TOKEN || "")
   .put(Routes.applicationCommands(process.env.CLIENT_ID || ""), { body: [] })
   .then(() => console.log("Successfully deleted all application commands."))
   .catch(console.error);
