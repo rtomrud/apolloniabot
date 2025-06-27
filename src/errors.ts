69c1add727f03aeafdf0c70fe28c8d2a6db97d50
Artem Rudenko <rtomrud@gmail.com>
2025-06-27 02:28:47 +0200

    Refactor errors into another file

---
+++ src/errors.ts
@@ -0,0 +1,20 @@
+export default {
+  VOICE_FULL: "I can't join your voice channel because it's full",
+  VOICE_CONNECT_FAILED:
+    "I can't join you because I can't connect to your voice channel",
+  VOICE_MISSING_PERMS:
+    "I can't join your voice channel because I don't have permission",
+  NO_RESULT: "I can't find that",
+  UNAVAILABLE_VIDEO: "I can't play that because it's unavailable",
+  UNPLAYABLE_FORMATS: "I can't play that because it's in an unplayable format",
+  NON_NSFW:
+    "I can't play that because it's age-restricted content and this is a SFW channel",
+  NOT_SUPPORTED_URL: "I can't play that because the website is unsupported",
+  CANNOT_RESOLVE_SONG: "I can't play that because the track is unresolved",
+  EMPTY_FILTERED_PLAYLIST:
+    "I can't play that because there's no valid track or there's only age-restricted content and this is a SFW channel",
+  EMPTY_PLAYLIST: "I can't play that because there's no valid track",
+
+  SPOTIFY_PLUGIN_NO_RESULT: "I can't find that",
+  YTDLP_ERROR: "I can't play that",
+};
--- src/events/interaction-create.ts
+++ src/events/interaction-create.ts
@@ -12,37 +12,16 @@ import {
 } from "discord.js";
 import { DisTubeError } from "distube";
 import commands from "../commands/index.js";
+import errors from "../errors.js";
 
 export const event = Events.InteractionCreate;
-
-const errorMessages = {
-  VOICE_FULL: "I can't join your voice channel because it's full",
-  VOICE_CONNECT_FAILED:
-    "I can't join you because I can't connect to your voice channel",
-  VOICE_MISSING_PERMS:
-    "I can't join your voice channel because I don't have permission",
-  NO_RESULT: "I can't find that",
-  UNAVAILABLE_VIDEO: "I can't play that because it's unavailable",
-  UNPLAYABLE_FORMATS: "I can't play that because it's in an unplayable format",
-  NON_NSFW:
-    "I can't play that because it's age-restricted content and this is a SFW channel",
-  NOT_SUPPORTED_URL: "I can't play that because the website is unsupported",
-  CANNOT_RESOLVE_SONG: "I can't play that because the track is unresolved",
-  EMPTY_FILTERED_PLAYLIST:
-    "I can't play that because there's no valid track or there's only age-restricted content and this is a SFW channel",
-  EMPTY_PLAYLIST: "I can't play that because there's no valid track",
-  SPOTIFY_PLUGIN_NO_RESULT: "I can't find that",
-  YTDLP_ERROR: "I can't play that",
-};
-
-const defaultErrorMessage = "Something went wrong, sorry";
 
 const handleError =
   (interaction: ChatInputCommandInteraction | MessageComponentInteraction) =>
   async (error: DisTubeError<string>) => {
-    const errorCode = error.errorCode as keyof typeof errorMessages;
+    const errorCode = error.errorCode as keyof typeof errors;
     if (
-      !errorMessages[errorCode] ||
+      !errors[errorCode] ||
       errorCode === "SPOTIFY_PLUGIN_NO_RESULT" ||
       errorCode === "YTDLP_ERROR"
     ) {
@@ -51,7 +30,7 @@ const handleError =
 
     const embeds = [
       new EmbedBuilder()
-        .setDescription(errorMessages[errorCode] || defaultErrorMessage)
+        .setDescription(errors[errorCode] || "Something went wrong, sorry")
         .setColor(Colors.Red),
     ];
     await interaction.reply({ embeds }).catch(async (error: DiscordjsError) => {
